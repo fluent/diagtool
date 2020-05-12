c16a337f361f9dbcf2c6a2674043b616382cfaa3
kubotat <tkubota@ctc-america.com>
2020-05-12 00:58:36 -0400

    latest

--- tool/maskutils_tester.rb
+++ tool/maskutils_tester.rb
@@ -1,7 +1,7 @@
 require '../lib/maskutils'
 include Diagtool
 
-#line = 'test01.demo.com,test02.demo.com:8080'
+line = 'test01.demo.com,test02.demo.com:8080 192.168.10.10 "192.168.10.10" \"192.168.10.10\" \'192.168.10.10\' //192.168.10.10'
 #line = 'text.txt'
 #line = 'org.freedesktop.hostname1'
 #line = '8.37.0-13.el8'
@@ -14,4 +14,4 @@ conf = {
 	}
 mask1 = MaskUtils.new(conf, 'DEBUG')
 puts mask1.mask_tdlog_inspector(line)
-puts mask1.get_masklog()
+#puts mask1.get_masklog()
